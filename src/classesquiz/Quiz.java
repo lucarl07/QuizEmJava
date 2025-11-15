f5f13cb4c13799ac0bf2006d07de3d88bb6d840e
lucarl07 <junitinho2007@gmail.com>
2025-11-15 19:04:21 -0300

    🚧 Em construção: estrutura da classe Quiz

--- src/classesquiz/Quiz.java
+++ src/classesquiz/Quiz.java
@@ -1,5 +1,13 @@
 package classesquiz;
 
 public class Quiz {
+    // Atributos da classe:
     static int quizzesJogados = 0;
+
+    // Atributos dos objetos:
+    int pontuacao = 0;
+
+    void adicionarPontuacao(int pontos) {
+        pontuacao += pontos;
+    }
 }
