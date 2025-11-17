ab0982176ceb70fbff52e5b2a022b64771dee14c
lucarl07 <junitinho2007@gmail.com>
2025-11-17 18:55:10 -0300

    Adicionar mais uma sobrecarga ao método SistemaLogs.registrarEvento

--- src/classesquiz/SistemaLogs.java
+++ src/classesquiz/SistemaLogs.java
@@ -4,13 +4,13 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class SistemaLogs {
-    private static String[] tiposEventoPadrao = {
+    private static String[] eventosPadrao = {
       "Programa iniciado.",
       "Programa finalizado subitamente",
       "Programa finalizado com êxito.",
-      "Quiz iniciado",
-      "Quiz finalizado subitamente.",
-      "Quiz finalizado com êxito."
+      "Quiz \"%s\" iniciado.",
+      "Quiz \"%s\" finalizado subitamente.",
+      "Quiz \"%s\" finalizado com êxito."
     };
     private static String formatoMsg = "[LOG] %s - %s %n";
     
@@ -18,15 +18,22 @@ public class SistemaLogs {
       DateTimeFormatter formatador = DateTimeFormatter.ofPattern("HH:mm:ss");
       return LocalTime.now().format(formatador);
     }
+    public void registrarEvento(int tipoEventoPadrao, String variavel) {
+        String evento = eventosPadrao[tipoEventoPadrao];
+        if (tipoEventoPadrao > 3) {
+            evento = String.format(evento, variavel);
+        }
+        System.out.printf(formatoMsg, obterStringHoraEvento(), evento);
+    }
     public void registrarEvento(int tipoEventoPadrao) {
         System.out.printf(
-          formatoMsg, 
-          obterStringHoraEvento(), tiposEventoPadrao[tipoEventoPadrao]
+            formatoMsg,
+            obterStringHoraEvento(), eventosPadrao[tipoEventoPadrao]
         );
     }
     public void registrarEvento(String mensagem) {
         System.out.printf(
-          formatoMsg, 
+          formatoMsg,
           obterStringHoraEvento(), mensagem
         );
     }
