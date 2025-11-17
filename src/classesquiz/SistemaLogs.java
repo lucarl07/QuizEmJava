8521fa09f047b66859ac981a5dc1d082d6710a33
Luiz Carlos Jr. <junitinho2007@gmail.com>
2025-11-17 11:14:17 -0300

    Adicionar sobrecarga de método em SistemaLogs

--- src/classesquiz/SistemaLogs.java
+++ src/classesquiz/SistemaLogs.java
@@ -4,12 +4,30 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class SistemaLogs {
-    protected int quantidadeEventos = 0;
-
+    private static String[] tiposEventoPadrao = {
+      "Programa iniciado.",
+      "Programa finalizado subitamente",
+      "Programa finalizado com êxito.",
+      "Quiz iniciado",
+      "Quiz finalizado subitamente.",
+      "Quiz finalizado com êxito."
+    };
+    private static String formatoMsg = "[LOG] %s - %s %n";
+    
+    private String obterStringHoraEvento() {
+      DateTimeFormatter formatador = DateTimeFormatter.ofPattern("HH:mm:ss");
+      return LocalTime.now().format(formatador);
+    }
+    public void registrarEvento(int tipoEventoPadrao) {
+        System.out.printf(
+          formatoMsg, 
+          obterStringHoraEvento(), tiposEventoPadrao[tipoEventoPadrao]
+        );
+    }
     public void registrarEvento(String mensagem) {
-        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("HH:mm:ss");
-        String horaEvento = LocalTime.now().format(formatador);
-
-        System.out.printf("[LOG] %s - %s %n", horaEvento, mensagem);
+        System.out.printf(
+          formatoMsg, 
+          obterStringHoraEvento(), mensagem
+        );
     }
 }
